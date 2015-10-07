749b2796a6caee3717446490d95143e1a5b2d007
Aelia Abbas <aelia.abbas@cityrdbclab5015.auckland.natcoll.ac.nz>
2015-10-07 15:43:39 +1300

    first

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// Do not load unil html is loaded
+
+$(document).ready(function(){
+	//hides p tags
+	$("p").hide();
+	//hides a specific id
+	$("#main-title").hide();
+	//hides a specific class
+	// $(".container").hide();
+	//hides a tags
+	$("a").hide();
+	//hides a tags
+	$("[href]").hide();
+
+	
+});
